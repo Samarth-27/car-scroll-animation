33d495e1016e1d0a2198b814f6fd0651bc9fd125
Samarth <samarth.sih25@gmail.com>
2026-02-25 12:36:56 +0530

    fix basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/car-scroll-animation",
+  assetPrefix: "/car-scroll-animation",
   images: {
     unoptimized: true,
   },
